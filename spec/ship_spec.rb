dce9cd2bbdf7a5e114eef944d78f946378975ada
Alex M <Alexm717@hotmail.com>
2022-05-17 14:07:21 -0700

    wrote test for hit

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -23,4 +23,15 @@ RSpec.describe(Ship) do
     expect(@cruiser.health).to(eq(3))
     expect(@cruiser.sunk?).to(eq(false))
   end
+
+  it("can check if a ship was hit") do
+    expect(@cruiser.health).to(eq(3))
+    expect(@cruiser.sunk?).to(eq(false))
+    @cruiser.hit
+    expect(@cruiser.health).to(eq((2)))
+    @cruiser.hit
+    expect(@cruiser.health).to(eq(1))
+    @cruiser.hit
+    expect(@cruiser.sunk?).to(eq(true))
+  end
 end
