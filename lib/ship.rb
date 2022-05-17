2334dd618043d7781953ebc7034ccf20c92610a9
Alex M <Alexm717@hotmail.com>
2022-05-17 13:49:03 -0700

    wrote methods to pass length/sunk? tests

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,14 +1,20 @@
 class Ship
-
-  attr_reader :name,
-              :health
+  attr_reader :name, :health
 
   def initialize(name, health)
     @name = name
     @health = health
   end
 
+  def length
+    @health
+  end
 
-
-
+  def sunk?
+    if @health == 0
+      true
+    else
+      false
+    end
+  end
 end
