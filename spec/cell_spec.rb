4fed20dc3a7ac7c3c3e69afe78d427fa0328128e
Alex M <Alexm717@hotmail.com>
2022-05-17 15:54:16 -0700

    added test for fired_upon and fire_upon

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,27 +1,36 @@
-require './lib/ship'
-require './lib/cell'
+require "./lib/ship"
+require "./lib/cell"
 
-RSpec.describe Cell do
-  before :each do
+
+RSpec.describe(Cell) do
+  before(:each) do
     @cell = Cell.new("B4")
     @cruiser = Ship.new("Cruiser", 3)
   end
 
-  it "exists" do
-    expect(@cell).to be_an(Cell)
+  it("exists") do
+    expect(@cell).to(be_an(Cell))
   end
 
-  it "has attributes" do
-    expect(@cell.coordinate).to eq("B4")
-    expect(@cell.ship).to eq(nil)
+  it("has attributes") do
+    expect(@cell.coordinate).to(eq("B4"))
+    expect(@cell.ship).to(eq(nil))
   end
 
-  it "can check if a cell is empty" do
-    expect(@cell.empty?).to eq(true)
+  it("can check if a cell is empty") do
+    expect(@cell.empty?).to(eq(true))
   end
 
-  it "can place the ship" do
+  it("can place the ship") do
     @cell.place_ship(@cruiser)
-    expect(@cell.empty?).to eq(false)
+    expect(@cell.empty?).to(eq(false))
+  end
+
+  it("can check if its been fired upon") do
+    @cell.place_ship(@cruiser)
+    expect(@cell.fired_upon?).to(eq(false))
+    @cell.fire_upon
+    expect(@cell.ship.health).to(eq(2))
+    expect(@cell.fired_upon?).to(eq(true))
   end
 end
