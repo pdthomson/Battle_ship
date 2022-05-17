dacfaa9a5b8d1c6dc4b7cc8ea5f054852f8e40b0
Paker Thomson <pdthomson1239@gmail.com>
2022-05-17 17:52:18 -0400

    wrote tests for existance and attributes of Cell class

---
+++ spec/cell_spec.rb
@@ -0,0 +1,19 @@
+require './lib/ship'
+require './lib/cell'
+
+RSpec.describe Cell do
+  before :each do
+    @cell = Cell.new("B4")
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  it "exists" do
+    expect(@cell).to be_an(Cell)
+  end
+
+  it "has attributes" do
+    expect(@cell.coordinate).to eq("B4")
+    expect(@cell.ship).to eq(nil)
+  end
+
+end
