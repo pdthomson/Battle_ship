c5e7dd65033e2eed178855783befaf782a91b48b
Paker Thomson <pdthomson1239@gmail.com>
2022-05-18 18:57:22 -0400

    Add method for render in Cell.rb

--- lib/cell.rb
+++ lib/cell.rb
@@ -27,7 +27,20 @@ class Cell
     if empty? == false
       @ship.hit
     end
-
     @fire_upon = true
+  end
+
+  def render(debug_ship = false)
+    if empty? == false && @fire_upon == true && @ship.sunk?
+      'X'
+    elsif @fire_upon == true && empty? == false
+      'H'
+    elsif debug_ship == true && empty? == false
+      'S'
+    elsif @fire_upon && empty? == true
+      'M'
+    else
+      '.'
+    end
   end
 end
