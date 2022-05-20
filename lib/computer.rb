a7139891ca399bf90983a63fd3e2bb45eab66620
Paker Thomson <pdthomson1239@gmail.com>
2022-05-20 13:54:56 -0400

    Add the method to place the submarine to the board

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,8 +1,12 @@
 class Computer
-  attr_reader :computer_board
+  attr_reader :computer_board,
+              :submarine,
+              :cruiser
 
   def initialize(board)
     @computer_board = board
+    @submarine = Ship.new("Submarine", 2)
+    @cruiser = Ship.new("Cruiser", 3)
   end
 
   def letters
@@ -17,5 +21,19 @@ class Computer
 
   def computer_coordinates
     letters + numbers
+  end
+
+  def place_submarine
+    loop do
+      submarine_coordinates = []
+      @submarine.length.times do
+        submarine_coordinates << computer_coordinates
+      end
+      submarine_coordinates.sort
+      if @computer_board.valid_placement?(@submarine, submarine_coordinates) == true
+        @computer_board.place(@submarine, submarine_coordinates)
+        break
+      end
+    end
   end
 end
